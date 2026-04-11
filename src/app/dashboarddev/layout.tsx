fb61c2a3d137503e76132e15275acf6e73da1090
Maintenance Web <maintenance.web@gmail.com>
2026-04-11 04:29:14 +0000

    ok

--- src/app/dashboarddev/layout.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from '@/app/dashboard/layout';
-
-export default function DashboardDevLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
